ac01a30cf3a280399eabd0301fa1265a7a22bca1
Alikhorshidj <121032889+Alikhorshidj@users.noreply.github.com>
2024-06-08 08:12:50 +0330

    Update main.py
    now you can use mouse with eye

--- eye_controlled_mouse/main.py
+++ eye_controlled_mouse/main.py
@@ -6,7 +6,7 @@ import pyautogui
 
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
-
+screen_w, screen_h = pyautogui.size()
 while True:
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)
@@ -21,6 +21,8 @@ while True:
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 0), 0)
             if id == 1:
-                pyautogui.moveTo(x, y)
+                screen_x = screen_w / frame_w * x
+                screen_y = screen_h / frame_h * y
+                pyautogui.moveTo(screen_x, screen_y)
     cv2.imshow('Eye Controlled Mouse', frame)
     cv2.waitKey(1)
